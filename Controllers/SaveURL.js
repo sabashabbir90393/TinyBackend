a064559e9121b35faeb26b259ee17715cbe3c39a
nosheenshabbirahmad435-arch <nosheenshabbirahmad435@gmail.com>
2026-09-11 16:50:29 +0500

    Update SaveURL.js

--- Controllers/SaveURL.js
+++ Controllers/SaveURL.js
@@ -8,12 +8,19 @@ try{
 const shortId = generateShortId(7);
 const newURL = new URLs({longUrl: longUrl, shortId: shortId});
 await newURL.save();
-const shortURL = `http://localhost:5050/${shortId}`;
+
+/* const shortURL = `http://localhost:5050/${shortId}`; */
+// Protocol aur Host dynamically get karein taake Railway domain auto-detect ho jaye
+    const host = req.get("host");
+    const protocol = req.protocol;
+    const shortURL = `${protocol}://${host}/${shortId}`;
 res.status(200).json({
+
+
     ok: true,
     shortURL: shortURL,
+    });
 
-});
 }catch(err){
 console.log(err);
 res.status(500).json({
